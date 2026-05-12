4e1e0e978869732cade7bf95d780b6cea2921357
pedromigueldev <pedro.miguel.e.idk@gmail.com>
2026-05-12 14:46:36 -0300

    feat: add custom directory creation

--- main.c
+++ main.c
@@ -2,8 +2,16 @@
 #include "vendor/mlib/mfile.h"
 
 int main(int argc, char** argv) {
+	if (argc < 2) {
+		fprintf(stderr, "Not enough arguments\n");
+    	return 1;
+    }
+    
 	char* path = NULL;
-	if (create_directory_tryfail(argc, argv, &path)) {
+    char* project_name = argv[1];
+   	char* where = argc == 3 ? argv[2] : NULL;
+	
+	if (mfile_mkdir_tryfail(where, project_name, &path)) {
 		fprintf(stderr, "ERROR: %s\n", path);
 		return 1;
 	}
