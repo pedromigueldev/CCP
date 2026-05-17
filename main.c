2d11ba7db5b8b9d110c19a7798d4a90fc8c622b0
pedromigueldev <pedro.miguel.e.idk@gmail.com>
2026-05-17 11:03:07 -0300

    misc

--- main.c
+++ main.c
@@ -1,46 +1,7 @@
 #include <stdio.h>
 #include "vendor/mlib/mfile.h"
-
-int create_makefile_tryfail (char** out_buffer, char* prject_name, char* path);
-int create_mainc_tryfail(char** out_buffer, char* path);
-
-int main(int argc, char** argv) {
-    char* project_name = argv[1] ? argv[1] : nullptr;
-   	char* project_path = argv[2] ? argv[2] : nullptr;
-    char* path = NULL;
-
-    if (project_name)
-    	path = MPRINT_FMT_OUT($(project_path != NULL ? project_path : ".")"/"$(project_name));
-    else {
-  		fprintf(stderr, "Ussage: <program-name> [project_path] [..flags]\n");
-    	return 1;
-    }
-
-    char* path_out = strdup(path);   
-	catch(mfile_mkdir_path, &path_out, 0755) {
-		fprintf(stderr, "Error: %s\n", unwrap_fail(path_out));
-		return 1;
-	}
-   	
-	char* makefile = NULL;
-   	catch(create_makefile, 	&makefile, project_name, path_out) {
-   		fprintf(stderr, "Error: %s\n", unwrap_fail(makefile));
-		return 1;
-   	}
-   	
-   	catch(create_mainc,		&makefile, path_out) {
-   		fprintf(stderr, "Error: %s\n", unwrap_fail(makefile));
-   		return 1;
-   	};
-   	
-	printf("Project at '%s' created successfully.\n", path_out);
-   	free(makefile);
-   	free(path_out);
-   	free(path);
-	return 0;
-}
-
 #define NL "\n"
+
 char* create_mainc_buffer () {
 	return MPRINT_FMT_OUT(
 		"#include <sdtio.h>			"NL
@@ -66,36 +27,43 @@ char* create_makefile_buffer (char* project_name) {
 	);
 }
 
-int create_makefile_tryfail (char** out_buffer, char* project_name, char* path) {
+int main(int argc, char** argv) {
+    char* project_name = argv[1] ? argv[1] : nullptr;
+   	char* project_path = argv[2] ? argv[2] : nullptr;
+    char* path = NULL;
+
+    if (project_name)
+    	path = MPRINT_FMT_OUT($(project_path != NULL ? project_path : ".")"/"$(project_name));
+    else {
+  		MPRINT_FMT("Ussage: <program-name> [project_path] <in_the_future: flags>");
+    	return 1;
+    }
+
+	catch(mfile_mkdir_path, &path, 0755) {
+		fprintf(stderr, "project: %s\n", unwrap_fail(path));
+		return 1;
+	}
+   	
+	char* out = NULL;	
 	char* makefile = create_makefile_buffer(project_name);
 	char* makefilePath = MPRINT_FMT_OUT($(path)"/makefile");
-	FILE* file = fopen(makefilePath, "w");
-	
-    if (file == NULL) {
-        wrap_fail(out_buffer) = strdup(strerror(errno));
-        return 1;
-    }
-    
-    fprintf(file, makefile);
-    fclose(file);
-    *out_buffer = makefile;
-    free(makefilePath);
-	return 0;
-}
+	catch(mfile_create, &out, makefilePath, makefile) {
+		fprintf(stderr, "makefile: %s\n", unwrap_fail(out));
+		return 1;
+	};
 
-int create_mainc_tryfail(char** out_buffer, char* path) {
-	char* mainc = create_mainc_buffer();
-	char* maincPath = MPRINT_FMT_OUT($(path)"/main.c");
-	FILE* file = fopen(maincPath, "w");
-	
-    if (file == NULL) {
-        wrap_fail(out_buffer) = strdup(strerror(errno));
-        return 1;
-    }
-    
-    fprintf(file, mainc);
-    fclose(file);
-    *out_buffer = mainc;
-    free(maincPath);
+   	char* mainc = create_mainc_buffer();
+  	char* maincPath = MPRINT_FMT_OUT($(path)"/main.c");
+   	catch(mfile_create,	&out, maincPath, mainc) {
+   		fprintf(stderr, "main: %s\n", unwrap_fail(out));
+   		return 1;
+   	};
+   	
+	printf("Project at '%s' created successfully.\n", path);
+	free(maincPath);
+	free(makefilePath);
+   	free(makefile);
+   	free(mainc);
+   	free(path);
 	return 0;
 }
